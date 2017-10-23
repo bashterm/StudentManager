44f48050fb18414e5e9164e1d3ff7d323e4b74cf
Aidan Sciortino <2001flyingpigs14@gmail.com>
2017-10-23 16:04:03 -0400

    Added tardy count to student class

--- src/Student.java
+++ src/Student.java
@@ -6,8 +6,12 @@ This class handles holding student data
 public class Student {
 	private String name; //makes a "name" variable that can't be accesed from outside this class.
 	private int id;//makes an "id" variable that can't be accesed from outside this class.
+	private int tardies = 0;
 	public Student(String name,int id) {//makes a constructor method that sets the data.
 		this.name=name;//set class variables equal to the class variables
 		this.id=id;
+	}
+	public void addTardy() {
+		tardies += 1;
 	}
 }
