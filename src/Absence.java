88bad8f53ffc52259fd6af5f482c968a8fe710b6
Aidan Sciortino <2001flyingpigs14@gmail.com>
2017-10-30 16:30:17 -0400

    Added code for Absences
    We made a arraylist for absences.

---
+++ src/Absence.java
@@ -0,0 +1,7 @@
+
+public class Absence {
+	private boolean excused;
+	public Absence(boolean excused) {
+		this.excused=excused;
+	}
+}
--- src/Student.java
+++ src/Student.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /*
 By Aidan Sciortino and Linden Burack
 2017
@@ -6,8 +8,16 @@ This class handles holding student data
 public class Student {
 	private String name; //makes a "name" variable that can't be accesed from outside this class.
 	private int id;//makes an "id" variable that can't be accesed from outside this class.
+	private ArrayList<Absence> absences= new ArrayList<Absence>();
+	
 	public Student(String name,int id) {//makes a constructor method that sets the data.
 		this.name=name;//set class variables equal to the class variables
 		this.id=id;
+	
 	}
+	public void addAbsence (Absence newAbsence) {
+		absences.add(newAbsence);
+	}
+		
+	
 }
